b4503a04aa09540765b321cd7a4aeb15b5479677
Alessandra Pereira <alessandralnpereira@gmail.com>
2020-10-26 09:01:24 -0300

    Updates app logic

---
+++ Caoculadora/Caoculadora/Model/Dog.swift
@@ -0,0 +1,22 @@
+//
+//  Dog.swift
+//  Caoculadora
+//
+//  Created by Alessandra Pereira on 26/10/20.
+//
+
+import Foundation
+
+// MARK: - Dog
+struct Dog {
+    let years: Int
+    let months: Int
+    let size: Size
+    
+    func humanAge() -> Int {
+        let humanYears = years * size.rawValue
+        let humanMonths = months * size.rawValue/12
+        
+        return humanYears + humanMonths
+    }
+}
---
+++ Caoculadora/Caoculadora/Model/Size.swift
@@ -0,0 +1,27 @@
+//
+//  Size.swift
+//  Caoculadora
+//
+//  Created by Alessandra Pereira on 26/10/20.
+//
+
+import Foundation
+
+// MARK: - Size
+enum Size: Int, CaseIterable {
+    case xsmall = 4
+    case small = 5
+    case medium = 6
+    case large = 7
+    case xlarge = 8
+    
+    var description: String {
+        switch self {
+        case .xsmall: return "Mini"
+        case .small: return "Pequeno"
+        case .medium: return "Médio"
+        case .large: return "Grande"
+        case .xlarge: return "Gigante"
+        }
+    }
+}
